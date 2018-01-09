b51ac303bcf1e09c5a71cb442669de9c4bd77f36
Blender2016 <devahmedazzab2014@gmail.com>
2018-01-09 18:26:49 +0000

    create new events with emit on server and client

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+var socket = io();
+socket.on('connect',function () {
+    console.log('Connected to the server');
+
+    socket.emit('createEmail' ,{
+        to :'example@example.com',
+        text: 'hello server',
+        createdAt:new Date().getTime()
+    });
+
+    socket.emit('createMessage',{
+        from: 'ahmed',
+        text:'hello samir'
+    });
+});
+socket.on('disconnect',function (){
+    console.log("user has disconnected from the server");
+});
+
+socket.on('newEmail',function(email){
+    console.log("new email",email);
+});
+
+socket.on('newMessage',function(message){
+    console.log("newMessage" ,message);
+});
+
+
--- server/server.js
+++ server/server.js
@@ -12,10 +12,35 @@ const server = http.createServer(app);
 const io = socketIO(server);
 io.on('connection',(socket)=>{
      console.log("new user connected to the server.");
+
      socket.on('disconnect',()=>{
          console.log("user disconnected from the server");
      });
+
+     socket.on('createEmail',(newEmail)=>{
+        console.log('createEmail ', newEmail);
+     });
+
+     socket.emit('newEmail',{
+         from : 'test@test.com',
+         text : 'hello world',
+         createdAt: '1234'
+     });
+
+     socket.on("createMessage",(newMessage)=>{
+         console.log("createMessage" ,newMessage);
+     });
+
+     socket.emit('newMessage',{
+         from :'ali',
+         text: 'hello everyone',
+         createdAt:12165
+     });
+
+
 });
+
+
 
 //builtin middlewares
 app.use(express.static(publicPath));
