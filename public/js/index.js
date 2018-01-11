7cdd3964b776a54f85a4ebf1156940cafb70284c
Blender2016 <devahmedazzab2014@gmail.com>
2018-01-12 01:23:00 +0200

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToButtom(){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+       console.log('Should scroll');
+       messages.scrollTop(scrollHeight);
+    }  
+}
+
 socket.on('connect',function () {
     console.log('Connected to the server');
 });
@@ -17,11 +31,7 @@ socket.on('newMessage',function(message){
         createdAt:formattedTime
     });
     jQuery('#messages').append(html);
-    // console.log("newMessage" ,message);
-    // var formattedTime = moment(message.createdAt).format('h:mm a');
-    // var li = jQuery('<li></li>');
-    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    // jQuery('#messages').append(li);
+    scrollToButtom();
 });
 socket.on('newLocationMessage',function(location){
     var formattedTime = moment(location.createdAt).format('h:mm a');
@@ -32,11 +42,7 @@ socket.on('newLocationMessage',function(location){
         createdAt:formattedTime
     });
     jQuery('#messages').append(html);
-    // var li = jQuery('<li></li>');
-    // var a = jQuery('<a target="_blank"> My current Location </a>');
-    // li.text(`${location.from} ${formattedTime}: `);
-    // a.attr('href',location.url);
-    // li.append(a);
+    scrollToButtom();
    
 });
 
